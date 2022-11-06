138480704fe749c6fa76e00d26b9c5986944434e
Mary Paul <marymc135@hotmail.com>
2022-11-05 23:42:42 -0400

    conact info

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer(
     setPage,
     Github,
-    Linkedin)
-    // Email)
+    Linkedin,
+    Email)
 
     {
         return (
@@ -12,7 +12,7 @@ function Footer(
             <div>
             <ul className="flex-row">
                 <li className="mx-2">
-                    <h1 id="projects">Contact Information</h1>
+                    <h1 id="contact">Contact Information</h1>
                     <a href="#github" onClick={() => setPage(<Github />)}></a>
                     <a className="font-italic hover:font-bold text-xl" href="https://github.com/marymc13">Github </a>
                 </li>
@@ -22,6 +22,13 @@ function Footer(
                 <li className="mx-2">
                     <a href="#linkedin" onClick={() => setPage(<Linkedin />)}></a>
                     <a className="font-italic hover:font-bold text-xl" href="www.linkedin.com/in/mary-paul-06740b199">LinkedIn </a>
+                </li>
+            </ul>
+
+            <ul className="flex-row">
+                <li className="mx-2">
+                    <a href="#email" onClick={() => setPage(<Email />)}></a>
+                    <a className="font-italic hover:font-bold text-xl" href="marymc135@gmail.com">Email </a>
                 </li>
             </ul>
         </div>
